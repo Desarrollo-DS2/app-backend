4acb451282495d28a570ee189102b3323efc7464
Christian Vargas <vargas.christian@correounivalle.edu.co>
2024-05-05 19:28:44 -0500

    Password backend verification

--- src/app/serializers/user_serializer.py
+++ src/app/serializers/user_serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from django.db import transaction
 from ..models.user_model import User
+import re
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,6 +11,25 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['email', 'user_id', 'first_name', 'middle_name', 'last_name', 'second_last_name',
                   'contact_number', 'address', 'city', 'birth_date', 'is_staff', 'is_superuser', 'password']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise ValidationError('La contraseña debe tener al menos 8 caracteres')
+
+        if not re.search('[A-Z]', value):
+            raise ValidationError('La contraseña debe tener al menos una letra mayúscula')
+
+        if not re.search('[a-z]', value):
+            raise ValidationError('La contraseña debe tener al menos una letra minúscula')
+
+        if not re.search('[0-9]', value):
+            raise ValidationError('La contraseña debe tener al menos un número')
+
+        if not re.search('[@_!#$%&*]', value):
+            raise ValidationError(
+                'La contraseña debe tener al menos uno de los siguientes caracteres especiales: @_!#$%&*')
+
+        return value
 
     @transaction.atomic
     def create(self, validated_data):
